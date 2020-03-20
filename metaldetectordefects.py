9ffd315b637fb1a34a843a3e4aa453f6a4c6ead4
pfernandezferna1993 <61467380+pfernandezferna1993@users.noreply.github.com>
2020-03-20 18:30:33 +0100

    #7 #13. [ADD] Defect detector on metal surfaces: metaldetectordefects.py

--- metaldetectordefects.py
+++ metaldetectordefects.py
@@ -8,25 +8,37 @@ Patricia Fernández Fernández
 PRÁCTICA: Detección de fallos en superficies metálicas.
 Clase principal: metal_detector_defect()
 '''
+from initialize import Initialize
+from defect import Defect as defect
+
 
 class Metal_detector_defect: 
     '''
         Clase que detecta defectos en piezas metálicas.
     '''
-    def __init__(self):
+    def __init__(self,pathdb,pathoutput):
         '''
             Constructor que carga el detector.
         '''
-        self.pathdb = ""
-        self.pathoutput = ""
+        self.pathdb = pathdb
+        self.pathoutput = pathoutput
 
-    def dectection(self,):
+    def dectection(self):
         '''
             Función a la que se le introduce una imágen de entrada y devuelve 
             a su salida un fichero xml.
-        '''
+        ''' 
+        #print(self.pathdb)
+        #print(self.pathoutput)
 
-        #1. Cargar y preprocesar los datos: llamar a clase initialize.
-        #2. Detectar defectos: llamar a la clase piece.
-        #3. Presentar resultados: llamar a la clase results.
-        #4. ¿? Evaluación: llamar a la clase evaluation.
+        #1. Cargar y preprocesar los datos:
+        initialize = Initialize(self.pathdb)
+        dirsImages = initialize.loadimages()
+
+        for file in dirsImages:
+            image = initialize.preprocesing(file)
+            #2. Detectar defectos:
+            detector = defect()
+            detector.defectdetector(image)
+            
+            #3. Presentar resultados: llamar a la clase results.
