bf9233836307689ddc825f89bef7fa96bda9076a
garaziAlfaro <61512136+garaziAlfaro@users.noreply.github.com>
2020-03-20 20:37:37 +0100

    #7 #13. [ADD] Added defect class: defect.py

--- defect.py
+++ defect.py
@@ -11,6 +11,11 @@ Clase que detecta un defecto en una pieza. Devuelve:
     - la fiabilidad de la clasificación del defecto
     - la localización del defecto
 '''
+import keras
+import numpy as np
+
+from keras_yolo3 import yolo3_one_file_to_detect_them_all as yolo 
+#from keras_yolo3 import train as yolotrain
 
 class Defect:
     '''
@@ -24,8 +29,24 @@ class Defect:
         self.tag = None
         self.fidelity = None
         self.location = []
+                    
+        # Load yolov3 model
+        model = yolo.make_yolov3_model()
+        weight_reader = yolo.WeightReader('./keras_yolo3/yolov3.weights')
+        weight_reader.load_weights(model)
+        print(type(model))
+        #model.save('model.h5')
+        self.model = model
+        
 
-    #1. Detector def defectos: def detect_fail
+    #1. Detector def defectos:
+    def defectdetector(self, image):
+        print(self.model)
+        #TO-DO:
+        yhat = self.model.predict(image, batch_size=None, verbose=0)
+        print(yhat)
+        print([a.shape for a in yhat])
+        #yolotrain._main_("../config.json")
         #1.1. Llamar al clasificador.
         #1.2. Llamar a location.
     #2. Clasificador: classificator_fail.
